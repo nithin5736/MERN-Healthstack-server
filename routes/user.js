03bf87b2f150af1f3c2d814946b7939b4672a749
SAI NITHIN <sainithin.g20@iiits.in>
2023-04-24 21:22:10 +0530

    Multer and Morgan removed

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const { updateProfile, uploadProfilePic, profileDetails, user, userList } = require('../controllers/userController');
-const { upload } = require('../middlewares/multerMiddleware');
+// const { upload } = require('../middlewares/multerMiddleware');
 
 const router = require('express').Router();
 
 router.get("/", userList);
 router.post('/updateprofile', updateProfile);
-router.post('/profilepic', upload.single('file'),uploadProfilePic);
+router.post('/profilepic', uploadProfilePic);
 router.post('/profiledetails',profileDetails)
 router.get('/:id', user)
 
